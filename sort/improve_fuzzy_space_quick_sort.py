34776e8de0830287af3e1e82fbb95643cb233402
ssjssh <ssj970763342@163.com>
2014-10-12 21:29:22 +0800

    通过在分区函数里面加上了合并的逻辑，使得快排算法的性能逐步提高

--- sort/improve_fuzzy_space_quick_sort.py
+++ sort/improve_fuzzy_space_quick_sort.py
@@ -5,8 +5,7 @@ from math import *
 模糊空间排序唯一需要考虑的问题就是空间的合并。
 其实也比较简单，因为排序之后的数组中：可以合并的元素都在附近，因此可以一次合并。
 复杂度是O(n),因此不影响总体的复杂度。
-但是可以看到这个算法并不会随着列表中重复元素的增加而提高性能。
-因为合并都是在排序之后才发生的。这个时候没法依靠减少元素个数的方法提高性能
+但是可以看到这个算法在分区之后会有一个合并，如果所有元素都相等,可以取得最好的性能O(n)
 """
 class Range(object):
 	"""docstring for Range"""
@@ -16,6 +15,9 @@ class Range(object):
 		self.__end=end
 
 	def __cmp__(self,other):
+		"""
+		这儿的定义简直是最核心的部分
+		"""
 		if self.__end<other.__start:
 			return -1
 		elif self.__start>other.__end:
@@ -41,6 +43,7 @@ def partition(li,start,end):
 	if li_len<2:
 		raise ValueError("list which lenght is less then 2 do not need to partition")
 	#使用最后一个元素作为分割点
+	print "end:",end
 	key=li[end]
 	middle_index=start
 	for x in xrange(start,end+1):
@@ -48,15 +51,26 @@ def partition(li,start,end):
 			li[middle_index],li[x]=li[x],li[middle_index]
 			middle_index+=1
 	li[end],li[middle_index]=li[middle_index],li[end]
-	return middle_index
+	#处理middle_index:middle_index有两种情况，一是被合并，二是合并其他元素。
+	result=[]
+	for i in xrange(start,end+1):
+		x=li[i]
+		if result and result[-1]==x:
+			result[-1]=result[-1].merge(x)
+		else:
+			result.append(x)
+		if i==middle_index:
+			middle_index=len(result)-1
+	li[:]=result[:]
+	return start,middle_index,len(result)-1
 
 def sort(li,start,end):
 	li_len=end-start+1
 	if li_len<2:
 		return li
-	middle_index=partition(li,start,end)
-	sort(li,start,middle_index-1)
-	sort(li,middle_index+1,end)
+	p_start,middle_index,p_end=partition(li,start,end)
+	sort(li,p_start,middle_index-1)
+	sort(li,middle_index+1,p_end)
 	result=[]
 	for x in li:
 		if result and result[-1]==x:
