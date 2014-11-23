96945cd3ba87f0d5b018fd9ccb3a2a87853299c3
ssjssh <ssj970763342@163.com>
2014-11-23 12:11:09 +0800

    在Random_quick_sort中添加门面方法

--- sort/random_quick_sort.py
+++ sort/random_quick_sort.py
@@ -7,8 +7,8 @@ def partition(li, start, end):
     li_len = end - start + 1
     if li_len < 2:
         raise ValueError("list which lenght is less then 2 do not need to partition")
-    #使用随机元素元素作为分割点并且把随机元素放在列表最后
-    #这样就可以不变动原来的逻辑了
+    # 使用随机元素元素作为分割点并且把随机元素放在列表最后
+    # 这样就可以不变动原来的逻辑了
     key_index = randint(start, end)
     key = li[key_index]
     li[key_index], li[end] = li[end], li[key_index]
@@ -29,6 +29,10 @@ def sort(li, start, end):
     sort(li, start, middle_index - 1)
     sort(li, middle_index + 1, end)
     return li
+
+
+def quick_sort(li):
+    return sort(li, 0, len(li) - 1)
 
 
 def main():
