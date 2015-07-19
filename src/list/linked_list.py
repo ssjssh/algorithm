c59d1dfa977e06d21217ab054e385a46e475b242
shengshijun <ssj970763342@163.com>
2015-07-19 21:38:10 +0800

    1, 修改双向链表中的bug. 2, 修改readme,制定下一步计划.

--- src/list/linked_list.py
+++ src/list/linked_list.py
@@ -2,7 +2,8 @@
 # -*- coding:utf-8 -*-
 
 """
-实现一个带哨兵的双向循环链表。一个哨兵是一个空对象，在双向链表实现中简化了改变链接的时候需要判断None的情况。
+实现一个带哨兵的双向循环链表。一个哨兵是一个空对象
+在双向链表实现中简化了改变链接的时候需要判断None的情况。
 实现这个链表的过程时刻记得更新插入和删除节点的前后节点的指针
 """
 from __builtin__ import object
@@ -12,36 +13,45 @@ class LinkedList(object):
     class Node(object):
         """docstring for LinkedList.Node"""
 
-        def __init__(self, key, prev, next):
+        def __init__(self, value, prev, next_node):
             super(LinkedList.Node, self).__init__()
-            self.key = key
+            self.value = value
             self.prev = prev
-            self.next = next
+            self.next_node = next_node
 
         def __str__(self):
-            return str(self.key)
+            return str(self.value)
 
     def __init__(self, *arg):
+        """
+        创建一个链表
+        :param arg: 可变参数,是列表的初始元素
+        :return:
+        """
         super(LinkedList, self).__init__()
+        # dump对象的prev指针始终指向列表的尾巴
+        # 而列表的最后一个元素的next指针则始终指向列表开头,整个列表就是一个环
         self.__dump = LinkedList.Node(None, None, None)
         self.__dump.prev = self.__dump
-        self.__dump.next = self.__dump
+        self.__dump.next_node = self.__dump
         self.__length = 0
-        for key in arg:
-            self.append(key)
+        for value in arg:
+            self.append(value)
 
     def insert(self, index, value):
         if index > self.__length:
             raise IndexError("can not insert beyond the list")
+
         cur_pos = 0
         cur_node = self.__dump
         while cur_pos < index:
-            cur_node = cur_node.next
+            cur_node = cur_node.next_node
             cur_pos += 1
+
         prev_node = cur_node.prev
         node = LinkedList.Node(value, prev_node, cur_node)
         cur_node.prev = node
-        prev_node.next = node
+        prev_node.next_node = node
         self.__length += 1
         return node
 
@@ -49,50 +59,46 @@ class LinkedList(object):
         last_node = self.__dump.prev
         node = LinkedList.Node(value, last_node, self.__dump)
         # 现在结尾的节点指向新加的结尾点
-        last_node.next = node
+        last_node.next_node = node
         self.__dump.prev = node
         self.__length += 1
-        # 处理空链表的情况
-        if self.__dump.next is self.__dump:
-            self.__dump.next = node
         return node
 
     def prepend(self, value):
-        node = LinkedList.Node(value, self.__dump, self.__dump.next)
-        self.__dump.next = node
-        self.__dump.next.prev = node
-        # 处理空链表的情况
-        if self.__dump.prev is self.__dump:
-            self.__dump.prev = node
+        node = LinkedList.Node(value, self.__dump, self.__dump.next_node)
+        self.__dump.next_node.prev = node
+        self.__dump.next_node = node
         self.__length += 1
         return node
 
     def search(self, value):
-        cur_node = self.__dump.next
-        while cur_node is not self.__dump and cur_node.key != value:
-            cur_node = cur_node.next
-        return cur_node.key
+        cur_node = self.__dump.next_node
+        while cur_node is not self.__dump and cur_node.value != value:
+            cur_node = cur_node.next_node
+        return cur_node.value
 
     def delete(self, value):
-        cur_node = self.__dump.next
-        while cur_node is not self.__dump and cur_node.key != value:
-            cur_node = cur_node.next
+        cur_node = self.__dump.next_node
+        while cur_node is not self.__dump and cur_node.value != value:
+            cur_node = cur_node.next_node
+
         # 如果不是空链表，那么就是查找到了相应的元素
         if cur_node is not self.__dump:
-            cur_node.prev.next = cur_node.next
-            cur_node.next.prev = cur_node.prev
+            cur_node.prev.next_node = cur_node.next_node
+            cur_node.next_node.prev = cur_node.prev
+            self.__length -= 1
 
-        return cur_node.key
+        return cur_node.value
 
     def __len__(self):
         return self.__length
 
     def __str__(self):
-        cur_node = self.__dump.next
+        cur_node = self.__dump.next_node
         li = []
         while cur_node is not self.__dump:
             li.append(str(cur_node))
-            cur_node = cur_node.next
+            cur_node = cur_node.next_node
         return '[' + ", ".join(li) + ']'
 
 
@@ -103,8 +109,18 @@ def main():
     li.delete(5050)
     print li
     li = LinkedList()
-    for x in xrange(1, 10):
+    for x in range(1, 10):
         li.insert(0, x)
+    print li
+
+    li = LinkedList()
+    for x in range(1, 10):
+        li.append(x)
+    print li
+
+    li = LinkedList()
+    for x in range(1, 10):
+        li.prepend(x)
     print li
 
 
